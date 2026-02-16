7f90a2c580c89532398512475ad39cd965a95156
shreyas7147 <shreyaspokalwar2004@gmail.com>
2026-02-16 15:52:02 +0530

    --

--- World.java
+++ World.java
@@ -1,5 +1,9 @@
 class World {
     public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        int sum=a+b;
+        System.out.println("Sum of a and b is: " + sum);
         System.out.println("Hello, World!");
     }
 }
